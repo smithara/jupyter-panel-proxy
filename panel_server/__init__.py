2171a59224578154103dce36e8df8b251a4edeb3
Ashley Smith <ashley.smith@magneticearth.org>
2021-03-22 13:39:28 +0000

    Change dashboard directory

--- panel_server/__init__.py
+++ panel_server/__init__.py
@@ -2,7 +2,7 @@ from glob import glob
 
 def setup_panel_server():
     return {
-        'command': ["panel", "serve", *glob('*.ipynb'), "--allow-websocket-origin=*", "--port", "{port}", "--prefix", "{base_url}panel"],
+        'command': ["panel", "serve", *glob('shared/Swarm_notebooks/dashboards/*.ipynb'), "--allow-websocket-origin=*", "--port", "{port}", "--prefix", "{base_url}panel"],
         'absolute_url': True,
         'timeout': 360,
     }
